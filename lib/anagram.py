6b7b9fce28c444e2783a89e6e6afc842cb48893c
+ <+>
2023-09-13 15:34:18 -0600

    finished lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, original_word):
+        self.original_word = original_word 
+        
+    def match(self, words):
+        anagrams = []
+        for potential_anagram in words:
+            if(sorted(potential_anagram) == sorted(self.original_word)):
+                anagrams.append(potential_anagram)
+        return anagrams
+                
+        
